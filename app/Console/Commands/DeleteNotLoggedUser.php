f8452c57f13908fb36b825b963172efaa0430e87
Pie Dev <jimipulsar@gmail.com>
2023-02-21 14:27:07 +0100

    update cron

--- app/Console/Commands/DeleteNotLoggedUser.php
+++ app/Console/Commands/DeleteNotLoggedUser.php
@@ -32,7 +32,7 @@ class DeleteNotLoggedUser extends Command
 
         $lastActivity = User::query()
             ->whereNotNull('last_login_at')
-            ->where('last_login_at', '<', now()->subHours(24))
+            ->where('last_login_at', '<=', now()->subHours(24))
             ->first();
 
         if ($lastActivity) {
@@ -46,22 +46,22 @@ class DeleteNotLoggedUser extends Command
             $lastActivity->delete();
         }
 
-        $neverLoggedIn = User::query()
-            ->whereNull('last_login_at')
-            ->orderBy('created_at', 'desc')
-            ->first();
-
-        if ($neverLoggedIn) {
-            ArchivedUser::create([
-                'name' => $neverLoggedIn->name,
-                'email' => $neverLoggedIn->email,
-                'password' => Hash::make($neverLoggedIn->password),
-                'created_at' => $current_timestamp,
-                'updated_at' => $current_timestamp
-            ]);
-            $neverLoggedIn->delete();
-
-        }
+//        $neverLoggedIn = User::query()
+//            ->whereNull('last_login_at')
+//            ->orderBy('created_at', 'desc')
+//            ->first();
+//
+//        if ($neverLoggedIn) {
+//            ArchivedUser::create([
+//                'name' => $neverLoggedIn->name,
+//                'email' => $neverLoggedIn->email,
+//                'password' => Hash::make($neverLoggedIn->password),
+//                'created_at' => $current_timestamp,
+//                'updated_at' => $current_timestamp
+//            ]);
+//            $neverLoggedIn->delete();
+//
+//        }
         Log::info('Cron Job is running');
         $this->comment("Deleted {$lastActivity} not logged in the last 24 hours.");
 
